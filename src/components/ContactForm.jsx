ad22b645e1bd59b71d5f5da141660c7a143c44c5
Sober-69 <150891930+Sober-69@users.noreply.github.com>
2026-07-16 03:28:04 +0500

    Update Formspree endpoint in ContactForm component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -13,7 +13,7 @@ export default function ContactForm() {
     const data = Object.fromEntries(formData);
 
     try {
-      const res = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+      const res = await fetch("https://formspree.io/f/mkodgevz", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
